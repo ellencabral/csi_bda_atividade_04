310c338acb181dd021199616bb8853573603249d
Ellen <ellencabral2098@gmail.com>
2022-12-02 09:47:41 -0300

    first commit

--- src/components/Firebase/ProdutosDao.js
+++ src/components/Firebase/ProdutosDao.js
@@ -19,15 +19,17 @@ function getOrderByChild(order,db,callback){
     console.log(order);
     const refDB = ref(db,'produtos/');
     const consulta = query(refDB,orderByChild(order))
-    onChildAdded(consulta,callback)
+    onChildAdded(consulta,callback) // evento
 }
 
-function getFilterByChild(filter,value, db,callback){
-    //implement aqui
-    console.log(filter)
-}
+function getFilterByChild(filter,value,db,callback){ 
+    
+    const refDB = ref(db,'produtos/');
+    const consulta = query(refDB,orderByChild(filter),startAt(value))
+    onChildAdded(consulta,callback);
+}   
 
-function getMostExpensive(db,setValue,list){
+function getMostExpensive(db,setValue,list){ //startAt()
     // implement aqui
     /**
      *     Nesta função é necessário implementar o callback,
@@ -40,17 +42,49 @@ function getMostExpensive(db,setValue,list){
      * mais caro. Lembrando que list é um array, use os métodos para trabalhar
      * com arrays em JavaScript! Dica: usem reverse() ou unshift().
      * 
-     *  */ 
+     * setValue = uma função referencia do banco para alterar os dados da lista
+     * list = a lista pra alterar
+     * 
+     * fazer ordem reversa (aula 03) no array "list"
+     * 
+     * 
+     * 
+     * 
+     * ordenar pelo preço, retorna o objeto como array, e faz o reverse desse array 
+     * 
+     * setValue é somente quando tudo estiver pronto
+     * */ 
 
+    const refDB = ref(db,'produtos/')
+    const consulta = (query(refDB,orderByChild('preco')))
 
+    //function(){}
+    //()=>{}
+    //n=>n**2
+
+    let tempList = []
+    onChildAdded(consulta, dados=>{
+        tempList.push(dados.val())
+    })
+
+    list = tempList.reverse()
+    setValue(list)
 }
 
 function getMostCheap(db,callback){
-    //implemente aqui
+    
+    const refDB = ref(db,'produtos/');
+    const consulta = query(refDB,orderByChild('preco'),startAt(0))
+
+    onChildAdded(consulta,callback)
 }
 
-function getPriceRange(value, db,callback){//0--->limit
-    //implemente aqui
+function getPriceRange(value,db,callback){//0--->limit
+    
+    const refDB = ref(db,'produtos/');
+    const consulta = query(refDB,orderByChild('preco'),startAt(0),endAt(Number(value)))
+    
+    onChildAdded(consulta,callback)
 }
 
 export {getOrderByChild, getFilterByChild, getMostExpensive, getMostCheap, getPriceRange}
